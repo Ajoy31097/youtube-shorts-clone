2a5b977147f517bc7b731b307cf716c5cda5feb0
Ajoy31097 <123059362+Ajoy31097@users.noreply.github.com>
2024-02-25 00:31:17 +0530

    Initial Commit & Project Setup

---
+++ youtube-shorts/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Video from './Components/Video';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='video-player'>
+      <Video />
+      </div>
+    </div>
+  );
+}
+
+export default App;
