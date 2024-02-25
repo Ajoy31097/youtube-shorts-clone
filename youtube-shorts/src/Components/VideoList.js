a5998c954b541f6d30128be814fc69cfcc9a8ef1
Ajoy31097 <123059362+Ajoy31097@users.noreply.github.com>
2024-02-25 13:51:58 +0530

    Minor Modifications Done for All Device Responsiveness

--- youtube-shorts/src/Components/VideoList.js
+++ youtube-shorts/src/Components/VideoList.js
@@ -2,25 +2,27 @@ import video1 from "../Videos/video1.mp4";
 import video2 from "../Videos/video2.mp4";
 import video3 from "../Videos/video3.mp4";
 import video4 from "../Videos/video4.mp4";
+import video5 from "../Videos/video5.mp4";
 
 const VideoList = [
+
     {
         id: 1,
-        url: video1,
-        channel: "Nature 2.0",
-        description: "Shimmering ocean caresses the shore",
-        likes: 523,
-        dislikes: 78,
-        comments: 124
-    },
-    {
-        id: 2,
         url: video2,
         channel: "Castle of Sands",
         description: "Artist crafts sand castles against waves",
         likes: 1259,
         dislikes: 373,
         comments: "1K"
+    },
+    {
+        id: 2,
+        url: video1,
+        channel: "Nature 2.0",
+        description: "Shimmering ocean caresses the shore",
+        likes: 523,
+        dislikes: 78,
+        comments: 124
     },
     {
         id: 3,
@@ -39,6 +41,15 @@ const VideoList = [
         likes: 3324,
         dislikes: 542,
         comments: "1.8K"
+    },
+    {
+        id: 5,
+        url: video5,
+        channel: "WingsOfWater",
+        description: "Fallen Rain Drops",
+        likes: 5274,
+        dislikes: 984,
+        comments: "3.7K"
     }
 ];
 
